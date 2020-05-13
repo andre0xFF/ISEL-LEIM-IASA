d284a7743480a0b461d94421c7c666b76a6efc05
André Fonseca <afbfonseca@gmail.com>
2020-05-13 21:13:40 +0100

    Remove package

--- Projects/p1/src/Game/Environment/EnvironmentEvent.java
+++
@@ -1,6 +0,0 @@
-package game.environment;
-
-public enum EnvironmentEvent implements Stimulus {
-    EXIT, NOISE, SILENCE, ENEMY, FLEE, VICTORY, DEFEAT;
-}
-
--- Projects/p1/src/Game/Environment/Stimulus.java
+++
@@ -1,3 +0,0 @@
-package game.environment;
-
-public interface Stimulus {}
