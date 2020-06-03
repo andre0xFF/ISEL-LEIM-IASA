b8763735b49bdf0cd269c0c67e58a3d244f148dd
André Fonseca <afbfonseca@gmail.com>
2020-06-03 23:56:57 +0100

    Reorganize modules

---
+++ Projects/p2/src/problems/eightpuzzle/models/PuzzleState.java
@@ -0,0 +1,27 @@
+package problems.eightpuzzle.models;
+
+import puzzle.Puzzle;
+import search.engine.tree.State;
+
+public class PuzzleState extends State {
+
+    private Puzzle puzzle;
+
+    public PuzzleState(Puzzle puzzle) {
+        this.puzzle = puzzle;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.puzzle.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return this.puzzle.toString();
+    }
+
+    public Puzzle get_puzzle() {
+        return this.puzzle;
+    }
+}
